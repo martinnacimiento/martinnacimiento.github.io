64a682168a5f6dabe382c524df8439b34fed39e6
Nacimiento Martin <martinnacimiento98@gmail.com>
2020-04-21 21:48:16 +0000

    deploy

---
+++ js/about.fd5b70f7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},c=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("section",{staticClass:"section"},[n("div",{staticClass:"title has-text-centered"},[t._v("Sobre mí")])])}],a=n("2877"),i={},l=Object(a["a"])(i,s,c,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=about.fd5b70f7.js.map
---
+++ precache-manifest.fb9aea8f1492e19ee4cf92a1e6609840.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f08f4a2c0163b5a4a42",
+    "url": "/css/app.68caa58c.css"
+  },
+  {
+    "revision": "518742549a3cc6132468",
+    "url": "/css/chunk-vendors.b9dbcb27.css"
+  },
+  {
+    "revision": "b8dcd1f04c9a68ba28a6876b711c69dc",
+    "url": "/img/diciembre2017.b8dcd1f0.jpg"
+  },
+  {
+    "revision": "216e3ba323b25f09932339541792b027",
+    "url": "/img/martin2.216e3ba3.jpg"
+  },
+  {
+    "revision": "248db34a26a5e75874a8c0f0e7cc1b4f",
+    "url": "/img/me-recibi.248db34a.jpeg"
+  },
+  {
+    "revision": "ad454ed684cfd79f80812a9c8b3228c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "888bc12ccea4af1d0618",
+    "url": "/js/about.fd5b70f7.js"
+  },
+  {
+    "revision": "2f08f4a2c0163b5a4a42",
+    "url": "/js/app.2956ebb4.js"
+  },
+  {
+    "revision": "518742549a3cc6132468",
+    "url": "/js/chunk-vendors.15110418.js"
+  },
+  {
+    "revision": "5c8af7c8284e96fde8a7960b6a358bf8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
